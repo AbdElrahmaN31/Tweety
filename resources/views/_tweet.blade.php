354cc816106c1be9a8b7916683964f49a70df313
AbdElrahmaN31 <abdelrahmannashaat0@gmail.com>
2020-07-15 09:13:39 +0200

    design the time line

---
+++ resources/views/_tweet.blade.php
@@ -0,0 +1,10 @@
+<div class="flex p-4 border-b border-b-gray-400">
+    <div class="mr-2 flex-shrink-0">
+        <img src="https://i.pravatar.cc/50" class="rounded-full mr-2" alt="John Doe">
+    </div>
+    <div>
+        <h5 class="font-bold mb-4">John Doe</h5>
+        <p class="text-sm">In the dark , We come out and play We are its children, And were here to stay Running through , Hungry for strays No invitation, take me away Im not cruel, But thats still what you see Club to club, Come see this city with me Hungry for life, Without your pity I dont want it, But you give it</p>
+    </div>
+
+</div>
