de1c886c5cd8a5cc39774829589c86ec7cc91af0
AbdElrahmaN31 <abdelrahmannashaat0@gmail.com>
2020-07-16 10:10:50 +0200

    correct avatar  properties.

--- resources/views/_tweet.blade.php
+++ resources/views/_tweet.blade.php
@@ -1,6 +1,7 @@
 <div class="flex p-4 border-b border-b-gray-400">
     <div class="mr-2 flex-shrink-0">
-        <img src="https://i.pravatar.cc/50?u={{$tweet->user->email}}" class="rounded-full mr-2" alt="John Doe">
+        <img src="https://i.pravatar.cc/50?u={{$tweet->user->email}}"
+             class="rounded-full mr-2" alt={{ $tweet->user->name }}>
     </div>
     <div>
         <h5 class="font-bold mb-4">{{ $tweet->user->name }}</h5>
