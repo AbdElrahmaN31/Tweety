94ccab8f318a5f62e5c60b0bf232db19e1c53051
AbdElrahmaN31 <abdelrahmannashaat0@gmail.com>
2020-08-17 00:52:05 +0200

    fix follow issue with guest

--- resources/views/components/follow-button.blade.php
+++ resources/views/components/follow-button.blade.php
@@ -1,10 +1,12 @@
-@if(current_user()->isNot($user))
-    <form method="POST" action="{{ $user->path('follow') }}">
-        @csrf
-        <button
-            type="submit"
-            class="bg-blue-500 rounded-full shadow py-2 px-4 text-white text-xs">
-            {{ current_user()->following($user) ? 'Unfollow Me' : 'Follow Me' }}
-        </button>
-    </form>
-@endif
+@auth()
+    @unless ((current_user()->isNot($user)))
+        <form method="POST" action="{{ $user->path('follow') }}">
+            @csrf
+            <button
+                type="submit"
+                class="bg-blue-500 rounded-full shadow py-2 px-4 text-white text-xs">
+                {{ current_user()->following($user) ? 'Unfollow Me' : 'Follow Me' }}
+            </button>
+        </form>
+    @endunless
+@endauth()
